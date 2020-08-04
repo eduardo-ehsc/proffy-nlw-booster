d811dcdbeb280594d40fc95e0ec40648555224d5
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-08-04 12:20:32 -0300

    configurando o banco de dados
    Criada a conexão com o banco de dados e criada as tabelas

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,11 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+
+    return res.json({message: 'Hello World!'});
+})
+
 app.listen(8001);
