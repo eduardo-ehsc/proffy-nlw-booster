5a1c3ff1d704631b79f2d8630430932e2df124f5
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-08-06 16:26:53 -0300

    Estilização Landing Page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Landing from './src/pages/Landing';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Landing />
+      <StatusBar style="light" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
