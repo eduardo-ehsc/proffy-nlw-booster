5a2ebda272b457ce31f799f982ec70894c31c443
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-08-08 16:49:08 -0300

    Pegando os dados dos professores na API

--- mobile/src/Components/TeacherItem/index.tsx
+++ mobile/src/Components/TeacherItem/index.tsx
@@ -8,32 +8,44 @@ import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
 import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
 import whatsappIcon from '../../assets/images/icons/whatsapp.png';
 
-function TeacherItem(){
+export interface Teacher{
+    id: number;
+    name: string;
+    avatar: string;
+    bio: string;
+    cost: number;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
     return (
         <View style={styles.container}>
             <View style={styles.profile}>
                 <Image 
                     style={styles.avatar}
-                    source={{uri: 'https://github.com/eduardo-ehsc.png'}}
+                    source={{uri: teacher.avatar}}
                 />
 
                 <View style={styles.profileInfo}>
-                    <Text style={styles.name}>Eduardo Correia</Text>
-                    <Text style={styles.subject}>Programação</Text>
+                    <Text style={styles.name}>{teacher.name}</Text>
+                    <Text style={styles.subject}>{teacher.subject}</Text>
                 </View>
             </View>
 
             <Text style={styles.bio}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                {'\n'}
-                Commodi est debitis ab incidunt numquam ad quam aut blanditiis repellat corporis dolorem, magni aliquid, dolore, fuga quod quis? Earum, impedit aliquam.
+                {teacher.bio}
             </Text>
 
             <View style={styles.footer}>
                 <Text style={styles.price}>
                     Preço/Hora {'   '}
                     <Text style={styles.priceValue}>
-                        R$ 80,00
+                        R$ {teacher.cost},00
                     </Text>
                 </Text>
 
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -7,13 +7,32 @@ import styles from './styles';
 import { Feather } from '@expo/vector-icons'
 
 import PageHeader from '../../Components/PageHeader';
-import TeacherItem from '../../Components/TeacherItem';
+import TeacherItem, { Teacher } from '../../Components/TeacherItem';
+import api from '../../services/api';
 
 function TeacherList(){
+    const [teachers, setTeachers] = useState([]);
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+    
     const [isFiltersVisible, setIsFiltersVisible] = useState(false);
 
     function handleToggleFiltersVisible(){
         setIsFiltersVisible(!isFiltersVisible);
+    }
+
+    async function handleFiltersSubmit(){
+        const response = await api.get('classes', {
+            params: {
+                subject, 
+                week_day, 
+                time
+            }
+        });
+
+        setIsFiltersVisible(false);
+        setTeachers(response.data);
     }
 
     return (
@@ -32,6 +51,8 @@ function TeacherList(){
                     <View style={styles.searchForm}>
                         <Text style={styles.label}>Matéria</Text>
                         <TextInput 
+                            value={subject}
+                            onChangeText={text => setSubject(text)}
                             style={styles.input}
                             placeholder="Qual a matéria?"
                             placeholderTextColor="#c1bccc"
@@ -41,6 +62,8 @@ function TeacherList(){
                             <View style={styles.inputBlock}>
                                 <Text style={styles.label}>Dia da semana</Text>
                                 <TextInput 
+                                    value={week_day}
+                                    onChangeText={text => setWeekDay(text)}
                                     style={styles.input}
                                     placeholder="Qual dia?"
                                     placeholderTextColor="#c1bccc"
@@ -50,6 +73,8 @@ function TeacherList(){
                             <View style={styles.inputBlock}>
                                 <Text style={styles.label}>Horário</Text>
                                 <TextInput 
+                                    value={time}
+                                    onChangeText={text => setTime(text)}
                                     style={styles.input}
                                     placeholder="Que horas?"
                                     placeholderTextColor="#c1bccc"
@@ -57,7 +82,10 @@ function TeacherList(){
                             </View>
                         </View>
 
-                        <RectButton style={styles.submitButton}>
+                        <RectButton 
+                            style={styles.submitButton}
+                            onPress={handleFiltersSubmit}
+                        >
                             <Text style={styles.submitButtonText}>
                                 Filtrar
                             </Text>
@@ -74,13 +102,14 @@ function TeacherList(){
                     paddingBottom: 16
                 }}
             >
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {teachers.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem 
+                            key={teacher.id}
+                            teacher={teacher}
+                        />
+                    )
+                })}
             </ScrollView>
         </View>
     )
